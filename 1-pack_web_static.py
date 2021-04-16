670d157fbdcbb55654525783bfb81b1f79fd92d7
nicolasherrerac <nicolasherrerac20@gmail.com>
2021-04-15 23:21:08 -0500

    pep8

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 """script that generates a .tgz"""
 from datetime import datetime
-from fabric.api import local 
+from fabric.api import local
+
 
 def do_pack():
     """Create folder and file"""
     time = datetime.now().strftime("%Y%m%d%H%M%S")
-    local ("mkdir -p versions")
+    local("mkdir -p versions")
     file = "versions/web_static_{}.tgz" .format(time)
     var = local("tar -cvzf {} web_static" .format(file))
     if var.succeeded:
