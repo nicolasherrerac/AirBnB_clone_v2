e014aef3e4c65de2ed4505a30033b3cbcb07250e
nicolasherrerac <nicolasherrerac20@gmail.com>
2021-05-01 12:29:34 -0500

    pep8

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -7,16 +7,19 @@ from models.state import State
 
 app = Flask(__name__)
 
+
 @app.route('/states_list', strict_slashes=False)
 def displayhtml():
     """display html"""
     states = storage.all(State)
     return render_template('7-states_list.html', states=states)
 
+
 @app.teardown_appcontext
 def close(self):
     """Close session"""
     storage.close()
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
